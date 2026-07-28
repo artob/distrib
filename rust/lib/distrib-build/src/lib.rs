9a5e534998434c4330330d511222f4f032891cf8
Arto Bendiken <arto@bendiken.net>
2026-07-27 22:41:44 -0700

    Create the distrib-build crate.

---
+++ rust/lib/distrib-build/src/lib.rs
@@ -0,0 +1,49 @@
+// This is free and unencumbered software released into the public domain.
+
+//! Distrib helps you distribute your software.
+
+#![no_std]
+#![allow(unused)]
+#![forbid(unsafe_code)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+pub fn all() {
+    #[cfg(feature = "dart")]
+    dart();
+    #[cfg(feature = "js")]
+    js();
+    #[cfg(feature = "python")]
+    python();
+    #[cfg(feature = "ruby")]
+    ruby();
+    #[cfg(feature = "rust")]
+    rust();
+    #[cfg(feature = "wasm")]
+    wasm();
+}
+
+#[cfg(feature = "dart")]
+pub fn dart() {}
+
+#[cfg(feature = "js")]
+pub fn js() {}
+
+#[cfg(feature = "python")]
+pub fn python() {
+    pyo3_build_config::add_extension_module_link_args();
+}
+
+#[cfg(feature = "ruby")]
+pub fn ruby() {}
+
+#[cfg(feature = "rust")]
+pub fn rust() {}
+
+#[cfg(feature = "wasm")]
+pub fn wasm() {}
