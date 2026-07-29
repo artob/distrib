91e53353e76c825faf7ccc9200939ec3649fdca8
Arto Bendiken <arto@bendiken.net>
2026-07-29 00:44:39 -0700

    Export more `package_json_schema` types.

--- rust/lib/distrib-js/src/package.rs
+++ rust/lib/distrib-js/src/package.rs
@@ -1,3 +1,9 @@
 // This is free and unencumbered software released into the public domain.
 
-pub use package_json_schema::PackageJson;
+pub use package_json_schema::{
+    AdditionalFields, Binary, Bug, BundledDependencies, Directories, Dist, EsNext, Exports, Man,
+    PackageJson, Person, PersonObject, Private, PublishConfig, Repository, Type, Workspaces,
+};
+
+#[cfg(feature = "serde")]
+pub use serde_json::Value;
