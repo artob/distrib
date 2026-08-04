a314390b7e4aa7b45974adfa490bc4a608b359a3
Arto Bendiken <arto@bendiken.net>
2026-08-04 02:49:40 -0700

    Stub `distrib inspect`.

--- rust/lib/distrib/src/main.rs
+++ rust/lib/distrib/src/main.rs
@@ -1,9 +1,13 @@
 // This is free and unencumbered software released into the public domain.
 
 use clientele::{
-    StandardOptions, SysexitsError,
+    StandardOptions,
+    SysexitsError::{self, *},
+    crates::camino::Utf8PathBuf,
     crates::clap::{Parser, Subcommand},
 };
+use thiserror::Error;
+use tracing::{error, info, warn};
 
 /// Distrib helps you distribute your software.
 #[derive(Debug, Parser)]
@@ -18,9 +22,70 @@ struct Options {
 }
 
 #[derive(Debug, Subcommand)]
-enum Command {}
+enum Command {
+    /// Inspect the current package's metadata.
+    #[clap(aliases = ["describe"])]
+    Inspect {
+        /// The project directory to use [default: $PWD].
+        project: Option<Utf8PathBuf>,
 
-pub fn main() -> Result<(), SysexitsError> {
+        /// The output format to use.
+        #[clap(short, long, default_value = "json")]
+        output: String,
+    },
+}
+
+impl Default for Command {
+    fn default() -> Self {
+        Self::Inspect {
+            project: None,
+            output: "json".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ProgramError {
+    #[error("unknown --output format: {0}")]
+    UnknownOutputFormat(String),
+
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Exit(#[from] SysexitsError),
+
+    #[error(transparent)]
+    Other(#[from] Box<dyn core::error::Error>),
+}
+
+impl From<ProgramError> for SysexitsError {
+    fn from(error: ProgramError) -> Self {
+        use ProgramError::*;
+        match error {
+            Exit(code) => code,
+            _ => EX_SOFTWARE,
+        }
+    }
+}
+
+pub fn main() -> SysexitsError {
+    use ProgramError::*;
+
+    match run() {
+        Ok(()) => EX_OK,
+        Err(Exit(exit)) => exit,
+        Err(error) => {
+            // TODO: color coding
+            error!("{}: error: {}", env!("CARGO_PKG_NAME"), error);
+            error.into()
+        },
+    }
+}
+
+pub fn run() -> Result<(), ProgramError> {
+    use ProgramError::*;
+
     // Load environment variables from `.env`:
     clientele::dotenv().ok();
 
@@ -43,9 +108,48 @@ pub fn main() -> Result<(), SysexitsError> {
     }
 
     // Configure debug output:
-    if options.flags.debug {}
+    if options.flags.debug {
+        tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::DEBUG)
+            .init();
+    } else {
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stderr)
+            .compact()
+            .without_time()
+            .with_target(false)
+            .with_level(false)
+            .with_thread_ids(false)
+            .with_thread_names(false)
+            .with_file(false)
+            .with_line_number(false)
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env()) // respects RUST_LOG
+            .with_max_level(match options.flags.verbose {
+                0 => tracing::Level::ERROR,
+                1 => tracing::Level::WARN,
+                2 => tracing::Level::INFO,
+                3 => tracing::Level::DEBUG,
+                _ => tracing::Level::TRACE,
+            })
+            .init();
+    }
 
-    // TODO: match options.command.unwrap() {}
+    let mut result = Ok(());
 
-    Ok(())
+    match options.command.unwrap_or_default() {
+        Command::Inspect { project: _, output } => {
+            // TODO: implement `distrib inspect`
+
+            match output.as_str() {
+                "json" => {
+                    // TODO: implement JSON output
+                },
+                _ => {
+                    return Err(UnknownOutputFormat(output));
+                },
+            }
+        },
+    };
+
+    result
 }
